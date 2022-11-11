9b764222770762189fac93d6a0cdba8a1a9cba46
eunji <qkrdmswl1018@naver.com>
2022-11-11 20:35:25 +0900

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
 import React, { useEffect, useState } from "react";
+import './App.css';
+import { Layout, Menu } from 'antd';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const { Header } = Layout;
+
 
 function App() {
   const [time, setTime] = useState(new Date());
@@ -13,17 +25,60 @@ function App() {
     };
   }, [timer]);
 
-  let iseven = true;
-  if (time.getTime() % 2 === 0) {
-    iseven = true;
-  } else {
-    iseven = false;
-  }
 
   return (
-    <div>
+    <Layout className="layout">
       <h3>현재 시간 : {time.toLocaleTimeString()}</h3>
-      <h3>홀짝 유무 = {iseven ? "짝" : "홀"} </h3>
+      <Router>
+        <Header>
+        <Menu theme="dark"
+         mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          items={[
+              {
+               key:1,
+               label: <Link to="/"> HOME</Link>,
+              },
+              {
+               key:2,
+               label: <Link to="/resume">이력서</Link>,
+              },
+              {
+               key: 3,
+               label: <Link to="/portfolio">포트폴리오</Link>,
+              },
+            ]}
+          />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/resume">이력서</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">포트폴리오</Link>
+            </li>
+          </ul>
+        </nav>
+        </Header>
+        <Layout.Content style={{ padding: "0 50px "}}>
+        <Routes>
+          <Route path="/portfolio" element={ <Portfolio />} />
+          <Route path="/resume" element={<Resume/>} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        </Layout.Content>
+      </Router>
+      </Layout>
+  );
+}
+
+
+const Resume = () => {
+  return (
+  <div>
       <h3>자기소개</h3>
       <h3>박은지</h3>
       <h3>학력</h3>
@@ -32,7 +87,15 @@ function App() {
         <li>토평중</li>
       </>
     </div>
-  );
+  )
+}
+
+const Portfolio = () => {
+  return <></>
+}
+const Home = () => {
+  return <>홈페이지
+  </>
 }
 
 export default App;
